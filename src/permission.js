4079ccab0725f0f17c1f4dc2bcd63bddf505729c
liujie <liu_anxin@163.com>
2020-01-04 16:02:51 +0800

    fix

--- src/permission.js
+++ src/permission.js
@@ -22,11 +22,11 @@ router.beforeEach(async(to, from, next) => {
   const params = (isBlank(toPath) || ignoreParamPath.includes(toPath) ? '' : '?redirect=' + toPath)
   const title = isNotBlank(to.meta.title) ? (to.meta.title + ' - ' + defaultSettings.title) : defaultSettings.title
 
-  // 从本地获取 token, 如果没有值表示未登录, 导去登录(下一页不是登录则拼在参数上, 这样登录成功后可以导回来)
+  // 从本地获取 token, 如果没有值表示未登录, 则先调用退出(删除 token 和 vuex 中的数据)再导去登录(下一页不是登录则拼在参数上, 这样登录成功后可以导回来)
   // 有 token 值就从 vuex 中获取权限
   //   如果 vuex 中没有权限规则(按 F5 刷新一下就没了)则从本地(没有就去请求后台)获取用户信息并写入 vuex
   //     写入成功后导向下一个页面(如果下一页是登录页则导去主页)
-  //     如果获取后台接口时异常则先调用退出(删除 token 和 vuex 中的数据)再导去登录(下一页不是登录则拼在参数上, 这样登录成功后可以导回来)
+  //     如果获取后台接口时异常则先调用退出(删除 token 和 vuex 中的数据及请求后端接口退出)再导去登录(下一页不是登录则拼在参数上, 这样登录成功后可以导回来)
   //   如果 vuex 中有权限规则依次进行下面的处理
   //     如果下一页是登录页则导去主页
   //     如果下一页没有权限, 则弹一个提示给用户表示其无权访问后再导去主页
@@ -34,11 +34,11 @@ router.beforeEach(async(to, from, next) => {
 
   const token = getToken()
   if (isBlank(token)) {
+    await store.dispatch('logout', false)
     if (toPath === login) {
       document.title = title
       next()
     } else {
-      await store.dispatch('logout', false)
       next(login + params)
       NProgress.done()
     }
