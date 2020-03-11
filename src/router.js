9faba3ef4f1e66220d2f3cc95e00d6271302e422
liuanxin <liu_anxin@163.com>
2020-03-11 21:37:19 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Layout from '@/layout/index'
+import Layout from '@/layout'
 import { isNotBlank } from '@/utils'
 import { getLocalData } from '@/utils/auth'
 
