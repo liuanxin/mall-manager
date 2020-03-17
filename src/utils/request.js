c5b833500fac95e0cc60fa8b795c14366b52faa5
liujie <liu_anxin@163.com>
2020-03-17 15:20:35 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,7 +15,7 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
-      config.headers['X-Token'] = getToken()
+      config.headers['x-token'] = getToken()
     }
     return config
   },
