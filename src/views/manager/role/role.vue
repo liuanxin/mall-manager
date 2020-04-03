f7821cc2272b0676b3d1b32e738af747c3c428b9
liujie <liu_anxin@163.com>
2020-04-03 20:27:04 +0800

    fix

--- src/views/manager/role/role.vue
+++ src/views/manager/role/role.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="role-container">
+    <div class="buttons">
+      <el-button @click="getKeys">print id</el-button>
+      <el-button @click="resetChecked">清空</el-button>
+    </div>
     <el-tree
+      ref="tree"
       :data="treeData"
       node-key="id"
       show-checkbox
       :default-expanded-keys="checkedList"
       :default-checked-keys="checkedList"
-      :props="defaultProps">
-    </el-tree>
+      :props="defaultProps"></el-tree>
   </div>
 </template>
 
@@ -18,8 +22,29 @@ export default {
   data() {
     return {
       treeData: isNotTrue(process.env.VUE_APP_ONLINE) ? getMockMenus() : [],
-      checkedList: [ 12, 21 ],
+      checkedList: [ 12, 13, 21 ],
       defaultProps: { children: 'children', label: 'name' }
+    }
+  },
+  methods: {
+    getKeys() {
+      // 跟标签上的 ref="tree" 相对应
+      const tree = this.$refs.tree
+
+      const keysNotParent = tree.getCheckedKeys()
+      console.log('不包含父: ' + keysNotParent)
+
+      // 使用 set 去重
+      const set = new Set()
+      keysNotParent.forEach(key => {
+        tree.getNodePath(key).map(node => set.add(node.id))
+        set.add(key)
+      })
+      const keysIncludeParent = [...set] // 或者使用 Array.from(set)
+      console.log('包含父: ' + keysIncludeParent)
+    },
+    resetChecked() {
+      this.$refs.tree.setCheckedKeys([]);
     }
   }
 }
@@ -27,6 +52,6 @@ export default {
 
 <style scoped>
 .role-container {
-  margin: 0 10px;
+  margin: 10px 10px 0;
 }
 </style>
