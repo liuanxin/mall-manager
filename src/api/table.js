fbfa1e0a0bc118841b001c61e4cb68da2b4eba90
TY <liu_anxin@163.com>
2020-05-29 21:00:35 +0800

    fix

--- src/api/table.js
+++ src/api/table.js
@@ -9,3 +9,6 @@ const getList = (params) => {
 }
 
 export { getList }
+
+// 如果是 params 将使用表单发送请求, 如果是 data 则会使用 RequestBody 的方式进行请求
+// method 如果是 delete 无法使用表单发送请求, 只能使用 RequestBody 或者使用 PathVariable
