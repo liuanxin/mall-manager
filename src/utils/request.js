0148e825bb9f7885c40321d45fc7e2dbc70857b4
liuanxin <liu.anxin13@gmail.com>
2022-05-15 21:41:52 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -14,31 +14,35 @@ const serviceRequest = axios.create({
 })
 
 // 状态映射
-const statusMapping = {
+const STATUS_MAPPING = {
   success: 200, // 正常返回
   notLogin: 401, // 显示错误信息并跳到登录页
   notPermission: 403 // 显示错误信息并跳到主页
 }
 
 serviceRequest.interceptors.request.use(
-  (config) => {
-    // if (store.getters.token) {
-    //   config.headers['x-token'] = getToken()
-    // }
+  (req) => {
+    // 文件下载
+    if (isTrue(req.isDownLoadFile)) {
+      req.responseType = 'blob'
+    }
+    // 上传文件
+    if (isTrue(req.isUploadFile)) {
+      req.headers['Content-Type'] = 'multipart/form-data'
+    }
+    // mock 时, 将 POST /user/info 请求改成 GET /api/example/post-user-info.json
     if (isTrue(process.env.VUE_APP_MOCK)) {
-      const realMethod = config.method.toLowerCase()
-      const realUrl = config.url
-      // mock 时, 将 POST /user/info 请求改成 GET /api/example/post-user-info.json
-      config.method = 'GET'
-      config.url = '/api/example/' + realMethod + (realUrl.startsWith('/') ? '' : '-') + realUrl.replace(/\//g, '-') + '.json'
+      const realMethod = req.method.toLowerCase()
+      const realUrl = req.url
+      req.method = 'GET'
+      req.url = '/api/example/' + realMethod + (realUrl.startsWith('/') ? '' : '-') + realUrl.replace(/\//g, '-') + '.json'
     }
 
-    const start = formatDateTimeMs()
-    config.startRequest = start
+    req.startRequest = formatDateTimeMs()
     if (isNotTrue(process.env.VUE_APP_ONLINE)) {
-      console.debug(start + ' request config: ' + JSON.stringify(config))
+      console.debug(req.startRequest + ' request req: ' + JSON.stringify(req))
     }
-    return config
+    return req
   },
   (error) => {
     if (isNotTrue(process.env.VUE_APP_ONLINE)) {
@@ -52,39 +56,41 @@ serviceRequest.interceptors.request.use(
 // 1. HttpStatus 返回 400 500 这样的非 200 的错误码, 此种直接处理 response.message
 // 2. HttpStatus 返回 200 但返回的 json 数据是 { "code": 500, "msg": "xxx 错误" } 这样的格式
 serviceRequest.interceptors.response.use(
-  (response) => {
-    const res = response.data
-    if (toInt(res.code) === statusMapping.success) {
-      return res
+  (res) => {
+    if (isNotTrue(process.env.VUE_APP_ONLINE)) {
+      console.debug('[' + getData(res, 'config.startRequest') + ' --> ' + formatDateTimeMs() + '] res:(' + JSON.stringify(res) + ')')
+    }
+    const data = res.data
+    if (toInt(data.code) === STATUS_MAPPING.success) {
+      return data
     } else {
       // 上面的第 2 种方式
-      return handleError(res, getData(response, 'config.startRequest'), isTrue(getData(response, 'config.errorReturn')))
+      return handleError(data, isTrue(getData(res, 'config.errorReturn')))
     }
   },
   (error) => {
+    if (isNotTrue(process.env.VUE_APP_ONLINE)) {
+      console.error('[' + getData(error, 'config.startRequest') + ' --> ' + formatDateTimeMs() + '] response error:(' + JSON.stringify(error) + ')')
+    }
     // 上面的第 1 种方式
-    return handleError(error, getData(error, 'config.startRequest'), isTrue(getData(error, 'config.errorReturn')))
+    return handleError(error, isTrue(getData(error, 'config.errorReturn')))
   }
 )
 
-const handleError = (error, startRequest, errorReturn = false) => {
-  if (isNotTrue(process.env.VUE_APP_ONLINE)) {
-    console.error('[' + startRequest + ' --> ' + formatDateTimeMs() + '] response error: ' + JSON.stringify(error) + ', return error: ' + errorReturn)
-  }
-
+const handleError = (error, errorReturn = false) => {
   // toInt(date.code || data.response.status)
   const code = toInt(getData(error, 'code') || getData(error, 'response.status'))
   // data.msg || data.response.data.message || data.message
   const msg = getData(error, 'msg') || getData(error, 'response.data.message') || getData(error, 'message')
   const showMessage = defaultValue((code === 0 ? '接口无法请求, 网络有误或有跨域问题: ' : '') + msg, '错误码: ' + code)
-  if (code === statusMapping.notLogin) {
+  if (code === STATUS_MAPPING.notLogin) {
     // 未登录(401): 显示信息后退出并重新加载当前页
     MessageBox.alert(showMessage).finally(() => {
       store.dispatch('logout').then(() => {
         location.reload()
       })
     })
-  } else if (code === statusMapping.notPermission) {
+  } else if (code === STATUS_MAPPING.notPermission) {
     // 无权限(403): 显示信息后跳到主页
     MessageBox.alert(showMessage).finally(() => {
       router.replace({path: '/'}).catch((e) => {
